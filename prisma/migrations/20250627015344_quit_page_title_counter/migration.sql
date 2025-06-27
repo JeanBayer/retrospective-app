ba88daaccbbc2d5d7744261f5c68832f83572faf
Jean Bayer <jhanbayer@gmail.com>
2025-06-26 21:54:17 -0400

    feat: eliminar columna `page_title` de la tabla `counters` en la base de datos

---
+++ prisma/migrations/20250627015344_quit_page_title_counter/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `page_title` on the `counters` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "counters" DROP COLUMN "page_title";
